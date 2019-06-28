03c9c13ecce1e69f1ae5cd5d017491ab3d76f11c
Max Savery <saverymax@gmail.com>
2019-06-28 15:46:43 -0400

    remove make_package

--- make_package.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# Script to build wheel for BmCS package
-# Directories are hardcoded in commands below. Must be edited to work on your computer
-rm -r build
-rm -r dist
-python setup.py bdist_wheel
-cp -r dist ../BmCS_installation_env
